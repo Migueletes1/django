b600a9bc79fc8f66d1621fd1ab5d0a27635943d0
Miguel <miguel.ricaurte2020@gmail.com>
2025-07-16 12:20:45 -0500

    sql

--- members/models.py
+++ members/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
+class Member(models.Model):
+    firstname = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30)
+    phone = models.IntegerField(null=True)
+    joined_date = models.DateField(null=True)
+    email = models.EmailField(max_length=30, null=True)
+    gener = models.CharField(
+        max_length=1, choices=[("M", "Male"), ("F", "Female")], null=True
+    )
+    birth_date = models.DateField(null=True)
+
+
 # Create your models here.
